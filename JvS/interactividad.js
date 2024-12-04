9b9ab64735efa5c4a8f4bf503926e575bb5425a5
adrianchs <151898780+adrianchs@users.noreply.github.com>
2024-12-03 23:04:07 -0600

    Delete JvS directory

--- JvS/interactividad.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('nav ul li a').forEach(function(menuItem) {
-    menuItem.addEventListener('mouseenter', function() {
-        this.style.color = '#ff6600';
-    });
-    menuItem.addEventListener('mouseleave', function() {
-        this.style.color = 'white';
-    });
-});
